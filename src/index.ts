7230c332bd1376eeb75d3dfe40548eb5485f9827
jordanrios94 <jordanrios.software@gmail.com>
2021-03-31 12:44:26 +0100

    feat: created classes for user and company

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('yo!');
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+
+console.log(user);
+
+const company = new Company();
+console.log(company);
